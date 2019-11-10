9f011532c8f28d3a5d62cfd8de4397f8aab31e81
David Voong <voong.david@gmail.com>
2019-11-10 20:46:26 +0000

    new users chart

--- fga_dashboards/fga_dashboards/static/App.js
+++ fga_dashboards/fga_dashboards/static/App.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 // import {scaleX} from "d3";
 import React from 'react';
 import { render } from 'react-dom';
-import  { DauChart, DauChartComponent } from "./DauChart";
-
+import { DauChart, DauChartComponent } from "./DauChart";
+import { NewUsersChart, NewUsersChartComponent } from "./NewUsersChart";
 
 window.React = React;
 axios.defaults.xsrfCookieName = "csrf_token";
@@ -17,40 +17,52 @@ class App extends React.Component {
             data: {dau: null,},
             dauChart: new DauChart(),
             loading: true,
+            newUsersChart: new NewUsersChart(),
+            screenWidth: window.innerWidth,
         };
+        this.updateWindow = this.updateWindow.bind(this);
+        window.addEventListener('resize', this.updateWindow);
+    }
+
+    updateWindow(e) {
+        this.setState({screenWidth: window.innerWidth});
     }
 
     componentDidMount() {
-        const {dauChart} = this.state;
+        const {dauChart, newUsersChart} = this.state;
         dauChart.initialise();
+        newUsersChart.initialise();
         axios.get("/api/get-data?start=2019-01-01")
             .then(response => response.data)
             .then(data => {
                 var data = {...data};
                 data.dau.values = data.dau.values.map(d => [new Date(d[0]), d[1], d[2]]);
+                data.newUsers.values = data.newUsers.values.map(d => [new Date(d[0]), d[1], d[2]]);
                 return data;
             })
             .then(data => this.setState({data: data, loading: false}));
     }
 
     componentDidUpdate() {
-        const {data, dauChart} = this.state;
+        const {data, dauChart, newUsersChart} = this.state;
         dauChart.draw(data.dau);
+        newUsersChart.draw(data.newUsers);
     }
     
     render() {
-        const {data, dauChart, loading} = this.state;
+        const {data, dauChart, loading, newUsersChart} = this.state;
+
 
         let loadingComponent = '';
         if(loading == true){
-            loadingComponent= <h1>loading</h1>;
-        }
-        
+            loadingComponent= <p>loading</p>;
+        } 
         return (
             <div className="container main-container">
               <h1>Dashboards</h1>
               {loadingComponent}
-              <DauChartComponent dauChart={dauChart}/>
+              <DauChartComponent dauChart={dauChart} hidden={loading}/>
+              <NewUsersChartComponent newUsersChart={newUsersChart} hidden={loading}/>
             </div>
         );
     }
--- fga_dashboards/fga_dashboards/views.py
+++ fga_dashboards/fga_dashboards/views.py
@@ -54,6 +54,17 @@ select id, DeviceOS from FGAUserDevices
 '''
     df_devices = query_database('users', sql)
 
+    sql = '''
+select
+    Id,
+    createdAt
+
+from FGAUsers
+'''
+    df_users = query_database('users', sql)
+    df_users['createdAt'] = pd.to_datetime(df_users['createdAt'])
+    df_users['createdAt'] = pd.to_datetime(df_users['createdAt']).dt.date
+    
     df = pd.merge(df_logs, df_devices, left_on='userDevice', right_on='id')
     df['createdAt'] = pd.to_datetime(df['createdAt'])
     df = df.drop(['id', 'userDevice'], axis=1)
@@ -72,10 +83,43 @@ select id, DeviceOS from FGAUserDevices
     df_by_client = df_by_client.reset_index()
     df_by_client['date'] = df_by_client['date'].map(lambda t: t.isoformat())
 
-    df = pd.concat([df_all, df_by_client], axis=0)
+    df_dau = pd.concat([df_all, df_by_client], axis=0)
     data['dau'] = {
-        'headers': [c for c in df.columns],
-        'values': df.values.tolist(),
+        'headers': [c for c in df_dau.columns],
+        'values': df_dau.values.tolist(),
     }
 
+    df = pd.merge(
+        df_logs,
+        df_users,
+        left_on='currentUser',
+        right_on='Id',
+        suffixes=['_log', '_user']
+    )
+    df = pd.merge(
+        df,
+        df_devices,
+        left_on='userDevice',
+        right_on='id',
+        suffixes=['', '_device']
+    )
+    df['createdAt_log'] = pd.to_datetime(df['createdAt_log'])
+    df['createdAt_log'] = df['createdAt_log'].dt.date
+    df = df[df['createdAt_log'] == df['createdAt_user']]
+    df['createdAt_log'] = df['createdAt_log'].map(lambda t: t.isoformat())
+    df = pd.DataFrame(df[['createdAt_log', 'currentUser', 'DeviceOS']])
+    df = df.rename(columns={'createdAt_log': 'date', 'currentUser': 'dau', 'DeviceOS': 'client'})
+    df_all = df.groupby('date')[['dau']].nunique()
+    df_all = df_all.reset_index()
+    df_all['client'] = 'All'
+    df_all = df_all[['date', 'client', 'dau']]
+    df_client = df.groupby(['date', 'client'])[['dau']].nunique()
+    df_client = df_client.reset_index()
+    df_client = df_client[['date', 'client', 'dau']]
+    df_new_users = pd.concat([df_all, df_client], axis=0)
+    data['newUsers'] = {
+        'headers': [c for c in df_new_users.columns],
+        'values': df_new_users.values.tolist(),
+    }
+    
     return JsonResponse(data)
